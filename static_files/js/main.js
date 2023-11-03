fc5044fc11a9de04dd1ba74e54a89930cbb81cb8
chrispydev <christianowusu44@gmail.com>
2023-11-03 12:48:33 +0000

    feat(file name): working on the cart section

--- static_files/js/main.js
+++ static_files/js/main.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(function (data) {
           // Update cart icon
           document.querySelector('#cart__info').textContent = data.cart_count;
+          // Update cart items in the navbar
         });
     });
   }
@@ -53,32 +54,32 @@ document.addEventListener('DOMContentLoaded', function () {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       // Update cart count in the HTML element
       document.querySelector('#cart__info').textContent = data.total_quantity;
+      updateCartItems(data.cart_items);
     });
 
-  // add to cart button
-  var addToCartButton = document.querySelector('#detail__add');
-  addToCartButton.addEventListener('click', function () {
-    var productID = this.getAttribute('data-product-id');
-    var csrfToken = getCookie('csrftoken');
-
-    // AJAX request
-    fetch('/add-to-cart/' + productID + '/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'X-CSRFToken': csrfToken,
-      },
-      body: 'product_id=' + productID,
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        // Update cart icon
-        document.querySelector('#cart__info').textContent = data.cart_count;
-      });
-  });
+  // Function to update cart items in the navbar
+  function updateCartItems(cartItems) {
+    // console.log(cartItems);
+    // var cartItemsList = document.querySelector('#cart__items_list');
+    // cartItemsList.innerHTML = '';
+    // const html = `
+    // `;
+    // cartItems.forEach(function (item) {
+    //   var listItem = document.createElement('li');
+    //   listItem.textContent = item.product + ' (' + item.quantity + ')';
+    //   cartItemsList.appendChild(listItem);
+    // });
+  }
 });
+
+/* Set the width of the side navigation to 40% */
+function openNav() {
+  document.getElementById('mySidenav').style.width = '40%';
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById('mySidenav').style.width = '0';
+}
