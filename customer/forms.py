cc8b47c1737c3727562b23e6fa7a98398bf2a096
chrispydev <christianowusu44@gmail.com>
2023-11-20 22:35:03 +0000

    feat(file name): added password_reset functionality

--- customer/forms.py
+++ customer/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from phonenumber_field.formfields import PhoneNumberField
 from customer.models import Customer
+from django.utils.translation import gettext as _
+from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth import password_validation
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -12,6 +15,21 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
+class CustomSetPasswordForm(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+            field.help_text = None  # Remove help text if desired
+
+    def render_errors(self):
+        error_messages = []
+        for field_name, errors in self.errors.items():
+            field_label = self.fields[field_name].label
+            for error in errors:
+                error_messages.append(f"{field_label}: {error}")
+        return ", ".join(error_messages)
 
 
 
--- customer/views.py
+++ customer/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render, HttpResponse,redirect
+from django.shortcuts import render,redirect
 from django.views import View
 from django.contrib import messages
+from django.contrib.auth import login
 from customer.forms import UserRegisterForm
 
 class RegisterForm(View):
     def post(self, request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
+            user = form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
+            login(request, user)
             return redirect('product_home')
         else:
-            return redirect('register')
+            user_form = UserRegisterForm()  # Create a new form instance
+            template_name = 'customer/register.html'
+            return render(request, template_name, {'user_form': user_form, 'form': form})
 
     def get(self, request):
         user_form = UserRegisterForm()
